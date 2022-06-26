e558c13c678d452303353305b2f1b5ddce5dc713
waterhumanB <dpfflsk45@gmail.com>
2022-06-26 18:48:29 +0900

    feat: 숫자 증감 애니메이션 easeOut 추가

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react'
 
+const easeOutExpo = (x: number) => {
+  return x === 1 ? 1 : 1 - Math.pow(2, -10 * x)
+}
+
 export default function useCounter(end: number, start = 0, duration = 2000) {
   const [count, setCount] = useState(start)
   const frameRate = 1000 / 60
@@ -8,7 +12,7 @@ export default function useCounter(end: number, start = 0, duration = 2000) {
   useEffect(() => {
     let currentNumber = start
     const counter = setInterval(() => {
-      const progress = ++currentNumber / totalFrame
+      const progress = easeOutExpo(++currentNumber / totalFrame)
       setCount(Math.round(end * progress))
 
       if (progress === 1) {
